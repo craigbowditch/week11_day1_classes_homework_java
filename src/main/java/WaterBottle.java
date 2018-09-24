0a350c7e1ea6b7b2297b66b20feafb1859330b5d
Craig Bowditch <craigbowditch@hotmail.com>
2018-09-24 16:39:40 +0100

    Completes Water Bottle Class. MVP done

---
+++ src/main/java/WaterBottle.java
@@ -0,0 +1,22 @@
+public class WaterBottle {
+    private int volume;
+
+    public WaterBottle(int volume) {
+        this.volume = 100;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+    public int drink() {
+        return this.volume -= 10;
+    }
+
+    public int emptyBottle(){
+       return this.volume = 0;
+    }
+
+    public int fillBottle(){
+        return this.volume = 100;
+    }
+}
---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        int volume = waterBottle.getVolume();
+        assertEquals(100, volume);
+    }
+
+    @Test
+    public void canDrinkOneDrink(){
+        waterBottle.drink();
+        int volume = waterBottle.getVolume();
+        assertEquals(90, volume);
+    }
+
+    @Test
+    public void canDrinkMultipleDrinks(){
+        waterBottle.drink();
+        waterBottle.drink();
+        int volume = waterBottle.getVolume();
+        assertEquals(80, volume);
+    }
+
+    @Test
+    public void canEmptyBottle(){
+        waterBottle.emptyBottle();
+        int volume = waterBottle.getVolume();
+        assertEquals(0, volume);
+    }
+
+    @Test
+    public void canFillBottle(){
+        waterBottle.emptyBottle();
+        waterBottle.fillBottle();
+        int volume = waterBottle.getVolume();
+        assertEquals(100, volume);
+    }
+}
